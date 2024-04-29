c23e536d9f0ea799451ea7f6a369bcccd407c34e
Recep Pazarlı <receppazarli99@hotmail.com>
2024-04-29 10:59:23 +0300

    Ordering sınıfı için mediator dosyasında gerekli yapılar oluşturulmaya başlandı.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries
+{
+    public class GetOrderingByIdQuery : IRequest<GetOrderingByIdQueryResult> // burda tek bir data döneceği içi liste gerek kalmadı.
+    {
+        public int Id { get; set; }
+
+        public GetOrderingByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
