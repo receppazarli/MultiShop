4717d20f176273d7e1a6626b0cd113e3788a6016
Recep Pazarlı <receppazarli99@hotmail.com>
2024-05-04 11:57:56 +0300

    Yeni Basket servisi projesi eklendi.

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a28715f159e863c5f4263d94560374a52d3a05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c17f7c10683d83ecadaf8c1725905a4691d9310")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
