e18a8d9c609f946fff7eba72cc51b03c407fa89e
Recep Pazarlı <receppazarli99@hotmail.com>
2024-05-05 14:45:06 +0300

    HeadUILayout ve TobbarUILayout Componentleri yapıldı.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponent/_TopbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponent
+{
+    public class _TopbarUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
