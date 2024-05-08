7204e709171c47b7341495bff619fda53e307e52
Recep Pazarlı <receppazarli99@hotmail.com>
2024-05-08 17:34:16 +0300

    CategoryController üzerinde eklemeler yapıldı. AdminLayouy MainSEctionViewBag Componetn eklendi.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,10 +2,16 @@
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         public IActionResult Index()
         {
+            ViewBag.v0 = "Kategori İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+
             return View();
         }
     }
