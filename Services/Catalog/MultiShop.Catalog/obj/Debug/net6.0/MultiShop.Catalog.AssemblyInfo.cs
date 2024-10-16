7797c05e1239f932406a6dd5caf44bff858f8729
Recep Pazarlı <receppazarli99@hotmail.com>
2024-10-16 12:29:02 +0300

    Feature backend tarafı hazırlandı.

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b220d940fcbc71dfd532cfe0a6e28949cb2028a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b0e912cbbc6f140ab66024b05d2e0f8125fc19")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
