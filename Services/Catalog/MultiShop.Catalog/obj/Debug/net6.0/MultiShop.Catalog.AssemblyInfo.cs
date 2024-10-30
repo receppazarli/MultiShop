c3991bc42a9a15357397bf011402d8075fc715fc
Recep Pazarlı <receppazarli99@hotmail.com>
2024-10-30 16:20:51 +0300

    OfferDiscount backend kısmı yapıldı ve test edildi.

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94208a42754b49eb0b71465cf4f96244369bdfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ecff61820a3d32711c460f8d7cacb6f6ae23938")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
